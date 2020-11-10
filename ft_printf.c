e5327e197e18d0197d38f0d3563954c9d1c0b1f3
Eduardo Barbosa <eduduarduo@hotmail.com>
2020-11-09 21:12:29 -0300

    New files and read Flags

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/30 20:16:52 by edbarbos          #+#    #+#             */
-/*   Updated: 2020/10/30 17:57:45 by root             ###   ########.fr       */
+/*   Updated: 2020/11/09 12:33:04 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int ft_printf(const char *str, ...)
 	va_list	args;
 	int		i;
 	char	c;
+	t_flags flags;
 
 	count = 0;
 	i = 0;
@@ -28,6 +29,8 @@ int ft_printf(const char *str, ...)
 	if (str[i] == '%')
 		{
 			i++;
+			init_flags(&flags);
+			
 			if ( str[i] == 'c')
 			{
 				c = va_arg(args, int);
@@ -58,4 +61,29 @@ void init_flags(t_flags *flags)
 	flags->minus = 0;
 	flags->zero = 0;
 	flags->conversion =  '\0';
+}
+
+int		readflag(t_flags *flags, const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] == '-' || str[i] == '*' || str[i] == '.' || ft_isdigit(str[i]))
+	{
+		//
+		if (ft_isdigit(str[i]))
+			while (ft_isdigit(str[i]))
+			{
+				if(flags->prec == -1)
+					flags->width = (flags->width * 10) + (str[i] - '0');
+				if(flags->prec == 0)
+					flags->width = (flags->width * 10) + (str[i] - '0');
+				i++;
+			}
+		else
+			i++;	
+	}
+	flags->conversion = str[i];
+	flags->len = i;
+	return (i);
 }
--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -1,41 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/30 20:16:52 by edbarbos          #+#    #+#             */
-/*   Updated: 2020/11/09 12:32:26 by root             ###   ########.fr       */
+/*   Updated: 2020/11/09 12:31:47 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdarg.h>
-# include <stdio.h>
-
-
-typedef struct s_flags {
-	int		len;
-	int		width;
-	int		prec;
-	int		star;
-	int		minus;
-	int 	zero;
-	char	conversion;
-}				t_flags;
-
-int ft_printf(const char *, ...);
-
-int ft_putchar_ret(char c);
-
-void init_flags(t_flags *flags);
-
-int	ft_isdigit(int c);
-
-
-#endif
+int	ft_isdigit(int c)
+{
+	if ((c >= '0') && (c <= '9'))
+		return (1);
+	else
+		return (0);
+}
